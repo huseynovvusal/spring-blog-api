5b118fc1aa14d746aee751d090f8ee08fbb9809a
I529010 <deepak.jain04@sap.com>
2025-09-02 18:24:16 +0530

    Change api endpoint to match the required path

--- src/main/java/com/huseynovvusal/springblogapi/config/SecurityConfig.java
+++ src/main/java/com/huseynovvusal/springblogapi/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req -> req
-                                .requestMatchers("/api/auth/**")
+                                .requestMatchers("/auth/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
